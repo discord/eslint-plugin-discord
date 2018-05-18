c739e3cc2193d1ab58a5d660c68473e223727574
TheSisb <shadiisber@gmail.com>
2018-05-17 22:41:47 -0700

    Forgot to pass node to getSourceCode call

--- tests/lib/rules/react-import-style.js
+++ tests/lib/rules/react-import-style.js
@@ -4,7 +4,12 @@ const rule = require('../../../lib/rules/react-import-style');
 
 module.exports = ruleTester =>
   ruleTester.run('react-import-style', rule, {
-    valid: [`import * as React from 'react';`, `import A from 'A';`],
+    valid: [
+      `
+/* @flow */
+import * as React from 'react';`,
+      `import A from 'A';`,
+    ],
 
     invalid: [
       {
