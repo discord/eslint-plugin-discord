4b8f1f928887465b217942f126b36aca4bf8dfa8
Michael Greer <michael.greer@gmail.com>
2018-06-04 09:44:46 -0700

    Feature/add custom camelcasing (#2)
    * camelcasing rule which allows UNSAFE_ prefix
    
    * add camelcase to exports

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
   rules: {
     'react-import-style': require('./lib/rules/react-import-style'),
     'use-a11y-component': require('./lib/rules/use-a11y-component'),
+    'amelcase-deprecation-support': require('./rules/camelcase-deprecation-support'),
   },
 };
