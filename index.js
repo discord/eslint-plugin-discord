2e7b58cba9e2ccd9eb6c48934f12730112a5a48e
Michael Greer <michael.greer@gmail.com>
2018-06-04 09:50:10 -0700

    fix camelcase ref

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ module.exports = {
   rules: {
     'react-import-style': require('./lib/rules/react-import-style'),
     'use-a11y-component': require('./lib/rules/use-a11y-component'),
-    'amelcase-deprecation-support': require('./rules/camelcase-deprecation-support'),
+    'camelcase-deprecation-support': require('./lib/rules/camelcase-deprecation-support'),
   },
 };
