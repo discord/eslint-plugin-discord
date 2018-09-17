1ec72f2119474e3c6f723997293c4e19e2d3b775
Meishu <theme97@gmail.com>
2018-09-17 14:50:08 -0700

    add explicit-createref-type

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ module.exports = {
     'react-import-style': require('./lib/rules/react-import-style'),
     'use-a11y-component': require('./lib/rules/use-a11y-component'),
     camelcase: require('./lib/rules/camelcase-deprecation-support'),
+    'explicit-createref-type': require('./lib/rules/explicit-createref-type'),
   },
 };
---
+++ tests/lib/rules/explicit-createref-type.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const rule = require('../../../lib/rules/explicit-createref-type');
+const parser = require.resolve('babel-eslint');
+
+module.exports = ruleTester =>
+  ruleTester.run('explicit-createref-type', rule, {
+    valid: [
+      {
+        parser,
+        code: `
+/* @flow */
+
+import * as React from 'react';
+
+class Test {
+  a: {current: React.ElementRef<*> | null} = React.createRef();
+  b: * = React.createRef();
+  c = React.createElement();
+  d = {};
+}
+`,
+      },
+    ],
+
+    invalid: [
+      {
+        parser,
+        code: `
+/* @flow */
+import * as React from 'react';
+class Test { a = React.createRef(); }
+`,
+        errors: [{messageId: 'explicitCreateRefTypeMessage'}],
+      },
+    ],
+  });
